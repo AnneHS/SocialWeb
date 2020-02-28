9d6006242589b56f57d6b1b1152f5273df331cae
annehs <annehoogerduijn@gmail.com>
2020-02-28 15:37:20 +0100

    Pizza Hut sentiment analysis

--- extract_pizzahut/twitterData.py
+++ extract_pizzahut/twitterData.py
@@ -15,11 +15,11 @@ CONSUMER_KEY = 'kKEOC6n2BKBVsks5RH45BmiRw'
 CONSUMER_SECRET = 'rXrfijW6S2wxb4Mt4yVil36QDcZNawsotjBb8X9PQ2zejmX6mr'
 
 # to get the oauth credential you need to click on the 'Generate access token' button:
-OAUTH_TOKEN = '1225147204739964929-obkKVb4GndaWd3GnZY6aCLafFPtbD7' 
+OAUTH_TOKEN = '1225147204739964929-obkKVb4GndaWd3GnZY6aCLafFPtbD7'
 OAUTH_TOKEN_SECRET = '7xNKPaSPQHfMzPeiD8JIemKg8dCQOBa3jp4Tx0922GGNP'
 auth = twitter.oauth.OAuth(OAUTH_TOKEN, OAUTH_TOKEN_SECRET,CONSUMER_KEY, CONSUMER_SECRET)
 twitter_api = twitter.Twitter(auth=auth)
-print(twitter_api) 
+print(twitter_api)
 
 
 # In[37]:
@@ -61,9 +61,8 @@ print(tweets_text)
 import csv
 
 rows = zip(tweets_time, tweets_id, tweets_text)
-newPath = "2127_data.csv"
+newPath = "test.csv"
 with open(newPath, "w") as f:
     writer = csv.writer(f)
     for row in rows:
         writer.writerow(row)
-
