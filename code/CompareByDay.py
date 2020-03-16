91b49790df14406ee664fe446a416bf0dc7ac1df
annehs <annehoogerduijn@gmail.com>
2020-03-16 10:34:51 +0100

    CompareByDay.py

--- code/CompareByDay.py
+++ code/CompareByDay.py
@@ -2,7 +2,23 @@ import pandas as pd
 import datetime
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
+'''
+Compare Dominos and Pizzahut at each day of a given week. Creats 3 barcharts:
+(i) Nr. of Tweets per day;
+(ii) Nr. of positive Tweets per day;
+(iii) Nr. of negative Tweets per day.
+
+Instructions
+Specify 3 parameters before running this script:
+(i) week (e.g. 'week1');
+(ii) name of Dominos csv-file;
+(iii) name of Pizzahut csv-file;
+where the csv-files already contain the sentiment info.
+
+The plots are saved to: ../SocialWeb/plots/weeki
+'''
 
 def Analyse(week, fileName):
     # read file
@@ -18,7 +34,6 @@ def Analyse(week, fileName):
         year, month, day = (int(x) for x in dt.split('-'))
         ans = datetime.date(year, month, day)
         weekday.append(ans.strftime("%A"))
-        #data['time'][i] = ans.strftime("%A")
 
     # Creates dictionary for number of tweets per weekday
     tweet_count = {'Monday':0, 'Tuesday':0, 'Wednesday':0, 'Thursday':0,
@@ -61,16 +76,20 @@ def Analyse(week, fileName):
 
     return tweet_count, pos_count, neg_count
 
-# Change week and filename here
+# Parameters
 week = 'week1'
-
 fileNameDominos = 'week1(24_01)dominos_data.csv'
 fileNamePizzaHut = 'week1(24_01)pizzahut_data.csv'
 
-
+# Get Tweet nfo
 dTweets, dPos, dNeg = Analyse(week, fileNameDominos)
 pTweets, pPos, pNeg = Analyse(week, fileNamePizzaHut)
 
+# Where to save the plots
+filePath='..//plots//' + week
+fileDir = os.path.dirname(os.path.realpath('__file__'))
+plotPath = os.path.join(fileDir, filePath) #'../data/test.csv')
+plotPath = os.path.abspath(os.path.realpath(plotPath))
 
 # labels and xticks for bar plots
 labels = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su']
@@ -85,9 +104,11 @@ ax.set_xticks(x)
 ax.set_xticklabels(labels)
 ax.set_ylabel('Tweets')
 ax.legend()
+plotName = 'TweetsByDay_' + week + '.png'
+plt.savefig(os.path.join(plotPath, plotName))
 plt.show()
 
-#  Bar chart: nr of positive tweets per weekday
+# Bar chart: nr of positive tweets per weekday
 fig, ax = plt.subplots()
 ax.bar(x-width/2, list(dPos.values()), width, label='Dominos')#align='center')
 ax.bar(x+width/2, list(pPos.values()), width, label='Pizza Hut')
@@ -95,9 +116,11 @@ ax.set_xticks(x)
 ax.set_xticklabels(labels)
 ax.set_ylabel('Positive Tweets')
 ax.legend()
+plotName = 'PosByDay_' + week + '.png'
+plt.savefig(os.path.join(plotPath, plotName))
 plt.show()
 
-#  Bar chart: nr of negative tweets per weekday
+# Bar chart: nr of negative tweets per weekday
 fig, ax = plt.subplots()
 ax.bar(x-width/2, list(dNeg.values()), width, label='Dominos')#align='center')
 ax.bar(x+width/2, list(pNeg.values()), width, label='Pizza Hut')
@@ -105,4 +128,6 @@ ax.set_xticks(x)
 ax.set_xticklabels(labels)
 ax.set_ylabel('Negative Tweets')
 ax.legend()
+plotName = 'NegByDay_' + week + '.png'
+plt.savefig(os.path.join(plotPath, plotName))
 plt.show()
